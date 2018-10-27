2d3e937a03db7b40cc9574c5d4aa2ceadb7e1f30
Stuart <stuarttottle@hotmail.com>
2018-10-27 21:17:02 +0100

    some test for about me

--- apps/web/src/app/about-me/components/about-me.component.ts
+++ apps/web/src/app/about-me/components/about-me.component.ts
@@ -3,9 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'stottle-about-me',
   template: `
-    <p>
-      about-me works!
-    </p>
+  <h2>Stuart Tottle</h2>
+  <h3>Software Engineer</h3>
+  <p>
+    I am blah...
+  </p>
+  <h3>What I do</h3>
+  <p>
+    My most recent experience is creating the front end for fanclub.com, 
+    a content sharing subscription platform with messaging chat.
+  </p>
+  <h4>Languages</h4>
+  <ul>
+    <li>JavaScript (ECMAScript 6)</li>
+    <li>Typescript 3</li>
+    <li>C# 6</li>
+  </ul>
+  <h4>Frameworks</h4>
+  <ul>
+    <li>JavaScript (ECMAScript 6)</li>
+    <li>Typescript 3</li>
+    <li>C# 6</li>
+  </ul>
+  <h4>Technologies</h4>
+  <ul>
+    <li>JavaScript (ECMAScript 6)</li>
+    <li>Typescript 3</li>
+    <li>C# 6</li>
+  </ul>
+  <h3>Emplyemnt History</h3>
+
   `,
   styles: []
 })
