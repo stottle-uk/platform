fe5554352d9677bf117438cd3c5d76444b4bc2ed
Stuart <stuarttottle@hotmail.com>
2018-10-27 21:47:08 +0100

    blog page

--- apps/web/src/app/blog/components/blog.component.ts
+++ apps/web/src/app/blog/components/blog.component.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'stottle-blog',
   template: `
-    <p>
-      blog works!
-    </p>
+  <h2>Medium Articles</h2>
+  <p>
+    Articles I have written and published on <a href="https://medium.com/@stuarttottle/">medium.com</a>. 
+    I write articles to confirm my understanding of a subject or technology and I hope others will find them useful.
+  </p>
+  <mat-list>
+    <mat-list-item *ngFor="let item of blogItems">
+      <mat-icon mat-list-icon>short_text</mat-icon>
+      <h4 mat-line>
+        <a [href]="item.link" target="_blank">
+          {{item.name}}
+        </a>
+      </h4>
+      <p mat-line>{{item.description}}</p>
+    </mat-list-item>
+  </mat-list>
   `,
   styles: []
 })
-export class BlogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class BlogComponent {
+  blogItems = [
+    {
+      name: 'Upload to Azure Blob Storage in Angular',
+      description:
+        'Upload to azure blob storage using microsofts azure javascript library with no server side code.',
+      link:
+        'https://medium.com/@stuarttottle/upload-to-azure-blob-storage-with-angular-7977e979496a'
+    },
+    {
+      name: 'Securion Pay - Node.js backend example',
+      description: 'Create subscriptions and take payments with securion pay.',
+      link:
+        'https://medium.com/@stuarttottle/securionpay-subscriptions-in-node-js-ff2c4521701f'
+    },
+    {
+      name: 'Integration Testing Identity Server',
+      description:
+        'Create integration tests for a .net core web api probject that uses identity server 4.',
+      link:
+        'https://medium.com/comparethemarket/asp-net-core-2-0-integration-testing-with-identity-server-7f328c22d969'
+    }
+  ];
 }
