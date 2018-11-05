9c991a0b6604d4d5dabb9194bd6d583a628a04ff
Stuart <stuarttottle@hotmail.com>
2018-11-05 21:22:29 +0000

    stateted on employment item

--- apps/web/src/app/about-me/+state/employment/employment.model.ts
+++ apps/web/src/app/about-me/+state/employment/employment.model.ts
@@ -1,3 +1,11 @@
 export interface Employment {
   id: string;
+  name: string;
+  content: EmploymentContent;
+  jobTitle: string;
+  location: string;
+}
+
+export interface EmploymentContent {
+  statements: string[];
 }
--- apps/web/src/app/about-me/components/employment-item-inner.component.ts
+++ apps/web/src/app/about-me/components/employment-item-inner.component.ts
@@ -4,9 +4,27 @@ import { Employment } from '../+state/employment/employment.model';
 @Component({
   selector: 'stottle-employment-item-inner',
   template: `
+  <mat-toolbar color="accent">
+
+    <button
+      type="button"
+      aria-label="Toggle sidenav"
+      mat-icon-button
+      (click)="drawer.toggle()">
+      <mat-icon aria-label="Side nav toggle icon">arrow_back</mat-icon>
+    </button>
+
+    <span fxFlex="100">{{name}}</span>
+
+  </mat-toolbar>
+
   <pre>{{employmentHistoryItem | json}}</pre>
   `
 })
 export class EmploymentItemInnerComponent {
   @Input() employmentHistoryItem: Employment;
+
+  get name(): string {
+    return this.employmentHistoryItem && this.employmentHistoryItem.name;
+  }
 }
