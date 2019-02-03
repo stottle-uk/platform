8e9972d88a095b4aa98f321a956e07e3135cc429
Stuart <stuarttottle@hotmail.com>
2019-02-03 20:35:48 +0000

    added event handler

--- libs/ngx-sendbird-wrapper/src/lib/services/sendbird.service.ts
+++ libs/ngx-sendbird-wrapper/src/lib/services/sendbird.service.ts
@@ -11,7 +11,13 @@ export const SEND_BIRD = new InjectionToken<SendBird.SendBirdInstance>(
   providedIn: 'root'
 })
 export class SendBirdService {
-  constructor(@Inject(SEND_BIRD) private sb: SendBird.SendBirdInstance) {}
+  private channelHandler = new this.sb.ChannelHandler();
+
+  constructor(@Inject(SEND_BIRD) private sb: SendBird.SendBirdInstance) {
+    this.channelHandler.onChannelChanged = channel => {
+      console.log(channel);
+    };
+  }
 
   connect(userId: string): Observable<SendBird.User> {
     return this.co(this.sb.connect.bind(this.sb), userId);
