9c65878802dbcd2b47718a1c5fc705427971c02c
Stuart <stuarttottle@hotmail.com>
2019-02-11 12:30:17 +0000

    shw uploade ile

--- libs/ngx-sendbird-wrapper/src/lib/components/message-list-item.component.ts
+++ libs/ngx-sendbird-wrapper/src/lib/components/message-list-item.component.ts
@@ -1,19 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit
+} from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'stottle-message-list-item',
   template: `
-    <p>
-      message-list-item works!
-    </p>
+    <mat-list-item>
+      <img matListAvatar [src]="mappedMessage.senderImg" />
+      <h3 matLine>{{ mappedMessage.sender }}</h3>
+      <p matLine>
+        <ng-container *ngIf="isUserMessage">{{ content }}</ng-container>
+        <ng-container *ngIf="isFileMessage">
+          <a [href]="file" target="_blank">Link</a>
+        </ng-container>
+        <span> {{ mappedMessage.date | date }} </span>
+        <span stottle-delete-message [message]="message">
+          delete
+        </span>
+      </p>
+    </mat-list-item>
   `,
-  styles: []
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessageListItemComponent implements OnInit {
+  @Input() message: SendBird.UserMessage | SendBird.FileMessage;
 
-  constructor() { }
-
-  ngOnInit() {
+  get isUserMessage(): boolean {
+    return this.message && this.message.isUserMessage();
   }
 
+  get isFileMessage(): boolean {
+    return this.message && this.message.isFileMessage();
+  }
+
+  get content(): string {
+    return (<SendBird.UserMessage>this.message).message;
+  }
+
+  get file(): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(
+      (<SendBird.FileMessage>this.message).url
+    );
+  }
+
+  get mappedMessage(): any {
+    return {
+      sender: this.message.sender.friendName,
+      senderImg: this.message.sender.profileUrl,
+      date: this.message.createdAt,
+      id: this.message.messageId
+    };
+  }
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {}
 }
--- libs/ngx-sendbird-wrapper/src/lib/components/messages-list-inner.component.ts
+++ libs/ngx-sendbird-wrapper/src/lib/components/messages-list-inner.component.ts
@@ -4,36 +4,15 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   selector: 'stottle-messages-list-inner',
   template: `
     <mat-list>
-      <mat-list-item *ngFor="let message of mappedMessages">
-        <img matListAvatar [src]="message.senderImg" />
-        <h3 matLine>{{ message.sender }}</h3>
-        <p matLine>
-          {{ message.content }}
-          <span> {{ message.date | date }} </span>
-          <span stottle-delete-message [message]="getMessage(message)">
-            delete
-          </span>
-        </p>
-      </mat-list-item>
+      <ng-container *ngFor="let message of messages">
+        <stottle-message-list-item
+          [message]="message"
+        ></stottle-message-list-item>
+      </ng-container>
     </mat-list>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagesListInnerComponent {
   @Input() messages: Array<SendBird.UserMessage | SendBird.FileMessage>;
-
-  get mappedMessages(): any[] {
-    return this.messages.map((m: SendBird.UserMessage) => ({
-      content: m.message,
-      sender: m.sender.friendName,
-      senderImg: m.sender.profileUrl,
-      date: m.createdAt,
-      id: m.messageId,
-      type: m.isUserMessage()
-    }));
-  }
-
-  getMessage(message: any): SendBird.UserMessage | SendBird.FileMessage {
-    return this.messages.find(m => m.messageId === message.id);
-  }
 }
