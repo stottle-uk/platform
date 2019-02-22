23ae62f1bd37821f1fc246c13a216bcaf596394f
Stuart <stuarttottle@hotmail.com>
2019-02-22 17:06:23 +0000

    cypress tests added

--- apps/web/src/app/home/components/home-inner.component.ts
+++ apps/web/src/app/home/components/home-inner.component.ts
@@ -3,30 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'stottle-home-inner',
   template: `
-  <div class="content">
-    <h1>stottle.uk</h1>
-    
-    <div fxLayout="column" fxLayout.gt-sm="row" class="cards">
-      <mat-card fxFlex="100" fxFlex.gt-sm="48"  fxFlexOffset.gt-sm="2" *ngFor="let card of cards" class="card-container">
-        <img mat-card-image [src]="card.image" [alt]="card.imageAlt">
-        <mat-card-content>
-          <h2>{{card.title}}</h2>
-          <p>
-            {{card.content}}
-          </p>
-        </mat-card-content>
-        <mat-divider></mat-divider>
-        <mat-card-actions>
-          <a mat-button [routerLink]="['/', card.button.link]">{{card.button.text}}</a>
-        </mat-card-actions>
-      </mat-card>
+    <div class="content">
+      <h1>stottle.uk</h1>
+
+      <div fxLayout="column" fxLayout.gt-sm="row" class="cards">
+        <mat-card
+          fxFlex="100"
+          fxFlex.gt-sm="48"
+          fxFlexOffset.gt-sm="2"
+          *ngFor="let card of cards"
+          class="card-container"
+        >
+          <img mat-card-image [src]="card.image" [alt]="card.imageAlt" />
+          <mat-card-content>
+            <h2>{{ card.title }}</h2>
+            <p>
+              {{ card.content }}
+            </p>
+          </mat-card-content>
+          <mat-divider></mat-divider>
+          <mat-card-actions>
+            <a
+              mat-button
+              [routerLink]="['/', card.button.link]"
+              [id]="card.id"
+              >{{ card.button.text }}</a
+            >
+          </mat-card-actions>
+        </mat-card>
+      </div>
     </div>
-  </div>
   `
 })
 export class HomeInnerComponent {
   cards = [
     {
+      id: 'home-about-me',
       image: '/assets/home/about-me.jpg',
       imageAlt: 'About me',
       title: 'About Me',
@@ -38,6 +50,7 @@ export class HomeInnerComponent {
       }
     },
     {
+      id: 'home-blog',
       image: '/assets/home/blog.jpg',
       imageAlt: 'Blog',
       title: 'Blog',
@@ -48,6 +61,7 @@ export class HomeInnerComponent {
       }
     },
     {
+      id: 'home-coding-kata',
       image: '/assets/home/coding-kata.jpg',
       imageAlt: 'Coding Kata',
       title: 'Coding Kata',
