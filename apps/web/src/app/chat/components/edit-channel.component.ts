5c7784da731db2e1cfaa757d08ce784101f8eb70
Stuart <stuarttottle@hotmail.com>
2019-03-25 11:31:27 +0000

    delete channel callbacl

--- apps/web/src/app/chat/components/edit-channel.component.ts
+++ apps/web/src/app/chat/components/edit-channel.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'stottle-edit-channel',
   template: `
-    <button stottleDeleteOpenChannel>Delete Channel</button>
+    <button stottleDeleteOpenChannel [callback]="deletedCallback">
+      Delete Channel
+    </button>
     <stottle-edit-current-channel></stottle-edit-current-channel>
   `
 })
-export class EditChannelComponent implements OnInit {
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+export class EditChannelComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  deletedCallback = () => this.router.navigate(['chat']);
 }
--- libs/ngx-sendbird-wrapper/src/lib/channels/directives/delete-open-channel.directive.ts
+++ libs/ngx-sendbird-wrapper/src/lib/channels/directives/delete-open-channel.directive.ts
@@ -1,18 +1,24 @@
-import { Directive, HostListener } from '@angular/core';
-import { take } from 'rxjs/operators';
+import { Directive, HostListener, Input } from '@angular/core';
+import { take, tap } from 'rxjs/operators';
 import { ChannelsViewStateService } from '../services/channels-view-state.services';
 
 @Directive({
   selector: '[stottleDeleteOpenChannel]'
 })
 export class DeleteOpenChannelDirective {
+  @Input()
+  callback: () => void;
+
   constructor(private vs: ChannelsViewStateService) {}
 
   @HostListener('click')
   onClick(): void {
     this.vs
       .deleteOpenChannel()
-      .pipe(take(1))
+      .pipe(
+        take(1),
+        tap(() => !!this.callback && this.callback())
+      )
       .subscribe();
   }
 }
