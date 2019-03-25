b9c9242806c0091bacffae4e9f1adc225fc51af1
Stuart <stuarttottle@hotmail.com>
2019-03-25 10:59:51 +0000

    tidy up

--- apps/web/src/app/chat/components/edit-channel.component.ts
+++ apps/web/src/app/chat/components/edit-channel.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'stottle-edit-channel',
   template: `
-    <p>
-      edit-channel works!
-    </p>
-
     <stottle-edit-current-channel></stottle-edit-current-channel>
-  `,
-  styles: []
+  `
 })
 export class EditChannelComponent implements OnInit {
   constructor() {}
